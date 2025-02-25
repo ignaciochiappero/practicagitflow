acf3d24b22f01c567ab3c259dbd66a834cb6a75c
nachochiappero <ignaciogabrielchiappero@gmail.com>
2025-02-24 22:42:58 -0300

    cambio hecho rama release

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-//Cambio para subir a producción
+//Cambio para subir a producción hecho en RELEASE
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
